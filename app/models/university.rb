292ac6109fdd8c0fd5e83fab2da5bb765c1a4389
Shaws <seanrichardparker@gmail.com>
2016-06-09 17:48:56 -0600

    Adding some model tests

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,18 @@
 class University < ActiveRecord::Base
   validates_presence_of :name, :size, :year_est
+
+  def self.by_name
+
+  end
+  def self.by_class_size(high = false)
+    if high
+      order(by_class: :desc)
+    else
+      order(:price)
+    end
+  end
+  
+  def self.by_year_est
+
+  end
 end
--- spec/models/university_spec.rb
+++ spec/models/university_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe University, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+
+    it {should respond_to :name}
+    it {should respond_to :class_size}
+    it {should respond_to :year_est}
+  end
 end
+
+  describe 'class methods' do
+    before(:each) do
+      @university1 = University.create(name: 'Merribee', class_size: 40, year_est: "1783" )
+      @university2 = University.create(name: 'Belford', class_size: 20, year_est: "1956" )
+      @university3 = University.create(name: 'Dunlan', class_size: 850, year_est: "1824" )
+    end
+
+    it 'sorts by name' do
+      universities = University.by_name
+      expect(universities).to eq([@university3, @university1, @university2])
+    end
+
+    it 'sorts by class_size asc' do
+      universities = University.by_class_size
+      expect(universities).to eq([@university2, @university1, @university3])
+    end
+
+    it 'sorts by year_est' do
+      universities.to_i.strip = University.by_year_est
+      expect(universities).to eq([@university1, @university2, @university3])
+    end
+
+  end
